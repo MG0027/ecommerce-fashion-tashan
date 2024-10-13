cb8cf0f89c349ad73a8e61857b1f510e18a394f7
Mudit Gandhi <mudit2752004@gmail.com>
2024-10-13 18:08:21 +0530

    edit

--- tback/app.js
+++ tback/app.js
@@ -23,7 +23,7 @@ app.use(express.urlencoded({extended: false}));
 
 // Serve static files from the React frontend app
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/uploads', express.static(path.join(__dirname, 'public', 'uploads')));
 mongoose
   .connect(config.mongoUrl)
   .then(() => console.log("MongoDB Connected"))
